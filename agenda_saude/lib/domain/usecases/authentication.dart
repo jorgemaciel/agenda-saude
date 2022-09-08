107f48f0f6c2403f11c8c6e118e32baa03c71599
jorgemaciel <jorge.maciel.jr@gmail.com>
2022-09-08 17:42:04 -0300

    fix: change Authentication usecase to receive a model

--- agenda_saude/lib/domain/usecases/authentication.dart
+++ agenda_saude/lib/domain/usecases/authentication.dart
@@ -1,8 +1,15 @@
 import '../entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    required String email,
-    required String password,
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  String email;
+  String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
   });
 }
